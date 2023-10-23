fe0f28d70c8f6297bd2221c66c4e2f8388d42a64
quietix <i.o.zashchyk@gmail.com>
2023-10-23 18:18:21 +0300

    progress

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -3,12 +3,49 @@ package com.example.lab2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewToRemove;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_1);
+        ToggleButton toggleButton = findViewById(R.id.toggleButton);
+        textViewToRemove = findViewById(R.id.text_box_to_move);
+
+        toggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ConstraintLayout parentLayout = (ConstraintLayout) textViewToRemove.getParent();
+                parentLayout.removeView(textViewToRemove);
+
+                ConstraintSet constraintSet = new ConstraintSet();
+                constraintSet.clone(parentLayout);
+
+                TextView newTextView = CreateTextView();
+                parentLayout.addView(newTextView);
+
+                constraintSet.connect(newTextView.getId(), ConstraintSet.END, parentLayout.getId(), ConstraintSet.END);
+                constraintSet.connect(newTextView.getId(), ConstraintSet.BOTTOM, parentLayout.getId(), ConstraintSet.BOTTOM);
+
+                constraintSet.applyTo(parentLayout);
+            }
+        });
+    }
+
+    private TextView CreateTextView(){
+        TextView newTextView = new TextView(this);
+        newTextView.setText("Text1");
+        newTextView.setId(View.generateViewId());
+        return newTextView;
     }
 }
