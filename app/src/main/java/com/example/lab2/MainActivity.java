06042261eb748694477b6827a0014e7825de825b
quietix <i.o.zashchyk@gmail.com>
2023-10-28 13:48:27 +0300

    spinner feature

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,24 +1,65 @@
 package com.example.lab2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Task2
     private TextView textViewToMove;
     private ToggleButton toggleButton;
+
+    // Task3
+    private Button addItemButton;
+    private Spinner spinner;
+    private ArrayAdapter<String> adapter;
+    private EditText textInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//         Task2();
+        Task3();
+    }
+
+    private void Task3(){
+        setContentView(R.layout.layout_2);
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item);
+
+        addItemButton = findViewById(R.id.add_item_button);
+        spinner = findViewById(R.id.spinner);
+        textInput = findViewById(R.id.input);
+
+        addItemButton.setOnClickListener(view -> {
+//            TextView newItem = new TextView(this);
+//
+//            Drawable drawable = getResources().getDrawable(R.drawable.border);
+//            newItem.setBackground(drawable);
+
+            String text = String.valueOf(textInput.getText());
+//            newItem.setText(text);
+
+            textInput.setText("");
+
+            adapter.add(text);
+            spinner.setAdapter(adapter);
+        });
+    }
+
+    private void Task2(){
         setContentView(R.layout.layout_1);
+
         toggleButton = findViewById(R.id.toggleButton);
         textViewToMove = findViewById(R.id.text_box_to_move);
 
