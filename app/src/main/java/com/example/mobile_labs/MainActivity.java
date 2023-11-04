d4563d799d2e12ecc8f0f0ec56cfe9c068567d16
quietix <i.o.zashchyk@gmail.com>
2023-11-04 12:00:38 +0200

    Task1. intent + toast are ready

--- app/src/main/java/com/example/mobile_labs/MainActivity.java
+++ app/src/main/java/com/example/mobile_labs/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.mobile_labs;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE = 1;
+    public TextView selectedCountryTextView;
+    private Button showCountriesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen_1);
+
+        showCountriesButton = findViewById(R.id.show_countries_list_button);
+        selectedCountryTextView = findViewById(R.id.selected_country_text_view);
+
+        showCountriesButton.setOnClickListener(this::showScreen2);
+    }
+
+    public void showScreen2(View view){
+        Intent intent1 = new Intent(getApplicationContext(), MainActivity2.class);
+        startActivityForResult(intent1, REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            String selectedCountry = data.getStringExtra(Intent.EXTRA_TEXT);
+            selectedCountryTextView.setText(selectedCountry);
+            Toast.makeText(getApplicationContext(), selectedCountry, Toast.LENGTH_SHORT).show();
+        }
     }
 }
---
+++ app/src/main/java/com/example/mobile_labs/MainActivity2.java
@@ -0,0 +1,60 @@
+package com.example.mobile_labs;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.screen_2);
+        generateCountriesList();
+    }
+
+    public void showScreen1(View view, String selectedCountry) {
+        Intent intent = new Intent();
+        intent.putExtra(Intent.EXTRA_TEXT, selectedCountry);
+        setResult(Activity.RESULT_OK, intent);
+        finish();
+    }
+
+
+    private void generateCountriesList() {
+        String[] countries = {
+                "Австрія",
+                "Албанія",
+                "Андорра",
+                "Бельгія",
+                "Болгарія",
+                "Боснія та Герцеговина",
+                "Велика Британія",
+                "Греція",
+                "Україна"
+        };
+
+        ListView countriesList = findViewById(R.id.countries_list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, countries);
+        countriesList.setAdapter(adapter);
+
+        makeListItemsTouchable(countriesList);
+    }
+
+    private void makeListItemsTouchable(ListView countriesList) {
+        countriesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedCountry = (String) parent.getItemAtPosition(position);
+                showScreen1(view, selectedCountry);
+            }
+        });
+    }
+}
