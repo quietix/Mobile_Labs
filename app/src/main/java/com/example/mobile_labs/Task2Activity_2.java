284bb5e64e200e9e93467d9fd0f82d1ed4307f72
quietix <i.o.zashchyk@gmail.com>
2023-11-05 10:31:04 +0200

    updated config to run activities different from MainActivity

---
+++ app/src/main/java/com/example/mobile_labs/Task2Activity_2.java
@@ -0,0 +1,14 @@
+package com.example.mobile_labs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Task2Activity_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task2_2);
+    }
+}
