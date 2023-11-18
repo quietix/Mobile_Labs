ecadeb48809dcc2763328ddb4e531475b75724e7
quietix <i.o.zashchyk@gmail.com>
2023-11-18 15:42:21 +0200

    task 1.1 is done (clickable options_menu with subitems)

--- app/src/main/java/com/example/mobile_labs/MainActivity.java
+++ app/src/main/java/com/example/mobile_labs/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.mobile_labs;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.widget.Toast.makeText;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    Toast currentToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.screen_1);
+        setContentView(R.layout.task_1);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        String message = Objects.requireNonNull(item.getTitle()).toString();
+        if (currentToast != null) {
+            currentToast.cancel();
+        }
+        currentToast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG);
+        currentToast.show();
+        return true;
     }
 }
